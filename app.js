433c8e995d8d10d791e5be56447bc99ead5da938
Vien Mai <56462929+vien-dev@users.noreply.github.com>
2023-03-14 22:29:58 +0100

    Example from community to insert a document

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 const { MongoClient } = require("mongodb");
-// Replace the uri string with your connection string.
+// Replace the uri string with your MongoDB deployment's connection string.
 const uri = "mongodb://127.0.0.1:27017";
 const client = new MongoClient(uri);
 async function run() {
   try {
-    const database = client.db('shopDB');
-    const products = database.collection('products');
-    // Query for a movie that has the title 'Back to the Future'
-    const query = { _id: 2 };
-    const product = await products.findOne(query);
-    console.log(product);
+    const database = client.db("insertDB");
+    const haiku = database.collection("haiku");
+    // create a document to insert
+    const doc = {
+      title: "Record of a Shriveled Datum",
+      content: "No bytes, no problem. Just insert a document, in MongoDB",
+    }
+    const result = await haiku.insertOne(doc);
+    console.log(`A document was inserted with the _id: ${result.insertedId}`);
   } finally {
-    // Ensures that the client will close when you finish/error
     await client.close();
   }
 }
