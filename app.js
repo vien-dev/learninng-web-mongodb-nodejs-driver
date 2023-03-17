08cdfb89e194834dcdb3779c5b82b97bc626358b
Vien Mai <56462929+vien-dev@users.noreply.github.com>
2023-03-17 22:52:05 +0100

    Correct the use of async and await when using mongoose

--- app.js
+++ app.js
@@ -14,83 +14,89 @@ const product = mongoose.Schema({
 const Products = mongoose.model("Products", product);
 
 async function run() {
-    mongoose.connect("mongodb://127.0.0.1:27017/shopDB").then(async function() {
-      await Products.deleteMany({}).then(function() {
-        console.log("All documents in shopDB.products are removed!");
-      }).catch(function() {
-        console.log("Cannot remove documents from shopDB.products");
-      });
-  
-      const pen = new Products({
-        _id: 1,
-        name: "Pen",
-        price: 1.2,
-        stock: 32
-      });
-      pen.save();
-  
-      var products = await Products.find({});
+  try {
+    await mongoose.connect("mongodb://127.0.0.1:27017/shopDB");
+    console.log("Connected to shopDB in MongoDB!");
+
+    await Products.deleteMany({});
+    console.log("All documents in shopDB.products are removed!");
+
+    const pen = new Products({
+      _id: 1,
+      name: "Pen",
+      price: 1.2,
+      stock: 32
+    });
+    await pen.save();
+    console.log("Created document for pen in shopDB.products");
+
+    await Products.insertMany([
+      {
+        _id: 2,
+        name: "Rubber",
+        price: 1.3,
+        stock: 43,
+        reviews: [
+          {
+            authorName: "Sally",
+            rating: 5,
+            review: "Best rubber ever!"
+          },
+          {
+            authorName: "John",
+            rating: 5,
+            review: "Awesome rubber!"
+          }
+        ]
+      },
+      {
+        _id: 3,
+        name: "Pencil",
+        price: 0.8,
+        stock: 12,
+        reviews: [
+          {
+            authorName: "Phuong",
+            rating: 4,
+            review: "Ok"
+          },
+          {
+            authorName: "Vien",
+            rating: 4,
+            review: "So so"
+          }
+        ]
+      }
+    ])
+    console.log("Rubber and Pencil are inserted to shopDB.products");
+
+    var products = await Products.find({});
+    console.log("Queried for all documents in shopDB.products");
+    if(products) {
       console.log(products);
-  
-      Products.insertMany([
-        {
-          _id: 2,
-          _name: "Rubber",
-          price: 1.3,
-          stock: 43,
-          reviews: [
-            {
-              authorName: "Sally",
-              rating: 5,
-              review: "Best rubber ever!"
-            },
-            {
-              authorName: "John",
-              rating: 5,
-              review: "Awesome rubber!"
-            }
-          ]
-        },
-        {
-          _id: 3,
-          name: "Pencil",
-          price: 0.8,
-          stock: 12,
-          reviews: [
-            {
-              authorName: "Phuong",
-              rating: 4,
-              review: "Ok"
-            },
-            {
-              authorName: "Vien",
-              rating: 4,
-              review: "So so"
-            }
-          ]
-        }
-      ]).then(function() {
-        console.log("Rubber and Pencil are inserted to shopDB.products");
-      });
-  
-      products = await Products.find({});
+    }
+    
+    products = await Products.find({stock: {$gt: 15}});
+    console.log("Queried for all documents with stock greater than 15 in shopDB.products");
+    if(products) {
       console.log(products);
-      
-      console.log("Query for products with stock greater than 15");
-      products = await Products.find({stock: {$gt: 15}});
+    }
+
+    await Products.updateOne({_id: 3}, {
+      $set: {
+        price: 0.90,
+        stock: 20
+      }
+    })
+    console.log("Updated price and stock for Pencil");
+
+    products = await Products.find({stock: {$gt: 15}});
+    console.log("Queried for all documents with stock greater than 15 in shopDB.products");
+    if(products) {
       console.log(products);
-  
-      Products.updateOne({_id: 3}, {
-        $set: {
-          price: 0.90,
-          stock: 20
-        }
-      }).then(function() {
-        console.log("Updated price and stock for Pencil");
-      });
-  
-      products = await Products.find({stock: {$gt: 15}});
-      console.log(products);
-    }).catch(err => console.log(err));
+    }
+  } catch (err) {
+    console.log(err);
+  }
 }
 run();
