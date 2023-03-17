b8a2bc3e5f8dc7c914b3265d3fd21567a6c25c25
Vien Mai <56462929+vien-dev@users.noreply.github.com>
2023-03-17 21:43:15 +0100

    Add a demo using mongoose for CRUD operations
    The same demo is carried out as of using pure MongoDB NodeJS driver:
    1. Remove any existing documents in shopDB.products
    2. Add 1st product (a Pen)
    3. Add 2nd and 3rd products
    4. Query all products
    5. Query only products with stock greater than 15 (only 1st and 2nd shown).
    6. Update 3rd product's price and stock.
    7. Query only products with stock greater than 15 (now all products are shown).

--- app.js
+++ app.js
@@ -1,62 +1,96 @@
-const { MongoClient } = require("mongodb");
-const assert = require("assert");
-// Replace the uri string with your connection string.
-const uri = "mongodb://127.0.0.1:27017";
-const client = new MongoClient(uri);
+const mongoose = require("mongoose");
+
+const product = mongoose.Schema({
+  _id: Number,
+  name: String,
+  price: Number,
+  stock: Number,
+  reviews: [{
+    authorName: String,
+    rating: Number,
+    review: String
+  }]
+});
+const Products = mongoose.model("Products", product);
+
 async function run() {
-  try {
-    const database = client.db('shopDB');
-    const products = database.collection('products');
-    await products.deleteMany({});
-    const docs = [
-        { _id: 1, name: 'Pen', price: 1.2, stock: 32 },
+    mongoose.connect("mongodb://127.0.0.1:27017/shopDB").then(async function() {
+      await Products.deleteMany({}).then(function() {
+        console.log("All documents in shopDB.products are removed!");
+      }).catch(function() {
+        console.log("Cannot remove documents from shopDB.products");
+      });
+  
+      const pen = new Products({
+        _id: 1,
+        name: "Pen",
+        price: 1.2,
+        stock: 32
+      });
+      pen.save();
+  
+      var products = await Products.find({});
+      console.log(products);
+  
+      Products.insertMany([
         {
-            _id: 2,
-            name: 'Rubber',
-            price: 1.3,
-            stock: 43,
-            reviews: [
-            { authorName: 'Sally', rating: 5, review: 'Best rubber ever!' },
-            { authorName: 'John', rating: 5, review: 'Awesome rubber!' }
-            ]
+          _id: 2,
+          _name: "Rubber",
+          price: 1.3,
+          stock: 43,
+          reviews: [
+            {
+              authorName: "Sally",
+              rating: 5,
+              review: "Best rubber ever!"
+            },
+            {
+              authorName: "John",
+              rating: 5,
+              review: "Awesome rubber!"
+            }
+          ]
         },
         {
-            _id: 3,
-            name: 'Pencil',
-            price: 0.8,
-            stock: 12,
-            reviews: [
-            { authorName: 'Phuong', rating: 4, review: 'Ok' },
-            { authorName: 'Vien', rating: 4, review: 'So so' }
-            ]
+          _id: 3,
+          name: "Pencil",
+          price: 0.8,
+          stock: 12,
+          reviews: [
+            {
+              authorName: "Phuong",
+              rating: 4,
+              review: "Ok"
+            },
+            {
+              authorName: "Vien",
+              rating: 4,
+              review: "So so"
+            }
+          ]
         }
-    ];
-    // this option prevents additional documents from being inserted if one fails
-    const options = { ordered: true };
-    const result = await products.insertMany(docs, options);
-
-    assert.equal(result.insertedCount, 3);
-
-    const query = {stock: {$gt: 15}};
-    await products.find(query).forEach((val) => { 
-      console.dir(val);
-    });
-
-    const filter = { _id: 3 };
-    const udpateDocument = {$set: {
-      price: 0.90,
-      stock: 20
-    }};
-    await products.updateOne(filter, udpateDocument);
-
-    //now query again
-    await products.find(query).forEach((val) => { 
-      console.dir(val);
-    });
-
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
+      ]).then(function() {
+        console.log("Rubber and Pencil are inserted to shopDB.products");
+      });
+  
+      products = await Products.find({});
+      console.log(products);
+      
+      console.log("Query for products with stock greater than 15");
+      products = await Products.find({stock: {$gt: 15}});
+      console.log(products);
+  
+      Products.updateOne({_id: 3}, {
+        $set: {
+          price: 0.90,
+          stock: 20
+        }
+      }).then(function() {
+        console.log("Updated price and stock for Pencil");
+      });
+  
+      products = await Products.find({stock: {$gt: 15}});
+      console.log(products);
+    }).catch(err => console.log(err));
 }
 run();
